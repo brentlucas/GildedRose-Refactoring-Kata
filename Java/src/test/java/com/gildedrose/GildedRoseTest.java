a3fe16741a0b50d07f28572ba1000a8201ec11dd
Brent Lucas <brent.lucas95@gmail.com>
2020-11-28 14:56:52 +0100

    add test cases until full coverage is reached

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,9 +10,17 @@ class GildedRoseTest {
 
     @Test
     void updateQuality() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1, 0, 11},
+                new Integer[]{0, 1, 49, 50});
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        Approvals.verify(app.items[0].toString());
+        return app.items[0].toString();
     }
 }
